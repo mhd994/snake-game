f64f3aecfd4c7036e2af375d3e38da7d435ae1bb
Mohamad Baker <mhdbaker999@gmail.com>
2025-07-17 16:15:10 +0300

    Update game.js

--- game.js
+++ game.js
@@ -14,10 +14,20 @@ import { outsideGrid } from './grid.js';
 let lastRenderTime = 0;
 let gameOver = false;
 const gameBoard = document.getElementById("game-board")
+let score = 0;
+function updateScore() {
+  document.getElementById('score').textContent = score;
+}
+if (onSnake(foodPosition)) {
+  expandSnake(EXPANSION_RATE); // or grow the snake
+  score += 1;                   // increase score
+  updateScore();                // update the score display
+  foodPosition = getRandomFoodPosition(); // new food
+}
 
 function main(currentTime){
     if(gameOver){
-       if(confirm('You Lost. Press Ok to Restart.')){
+       if(confirm('Game Over ! Press Ok to Restart.')){
         window.location = "./index.html";
        }
        return;
@@ -49,4 +59,5 @@ drawFood(gameBoard);
 function checkDeath(){
     gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
+
 
